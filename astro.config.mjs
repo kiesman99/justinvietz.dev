ca3255f55c8a66ca1c029ec2db2ffdaf6741bc49
Justin V <vietzi99@gmail.com>
2023-02-12 02:09:33 +0100

    chore: added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,8 +9,15 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import vercel from '@astrojs/vercel/static';
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://justinvietz.dev',
   // base: '/justinvietz.dev',
   integrations: [mdx(), sitemap(), react(), tailwind()],
+  output: "static",
+  adapter: vercel({
+    analytics: true
+  })
 });
