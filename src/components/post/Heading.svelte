32f7646435f0fafc42efbbc321c5b9956c9a7926
Justin V <vietzi99@gmail.com>
2023-02-17 15:32:31 +0100

    feat: under construction

--- src/components/post/Heading.svelte
+++ src/components/post/Heading.svelte
@@ -2,7 +2,15 @@
     export let title;
     export let pubDate;
     export let updatedDate;
+    export let draft;
 </script>
+
+{#if draft}
+<div class="border border-red-500 p-5 rounded-md dark:text-white flex flex-col items-center">
+  <p class="text-5xl ">THIS POST IS A DRAFT</p>
+<p>Please forgive grammatical and spelling errors. This article is currently under construction.</p>
+</div>
+{/if}
 
 <span class="flex flex-col justify-center items-center my-5 md:my-15">
   <h1 class="text-center text-5xl mb-7 mt-2 dark:text-white">{title}</h1>
