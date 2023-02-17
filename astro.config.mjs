d80c45016e9e570ea355c44498252471f6e5c9b7
Justin V <vietzi99@gmail.com>
2023-02-17 15:43:42 +0100

    fix: added vite aliases

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import path from 'path';
+
 // https://astro.build/config
 import react from "@astrojs/react";
 
@@ -22,5 +24,14 @@ export default defineConfig({
   output: "static",
   adapter: vercel({
     analytics: true
-  })
+  }),
+  vite: {
+    resolve: {
+      alias: {
+        "@components/*": path.resolve("./src/components/*"),
+        "@layouts/*": path.resolve("./src/layouts/*"),
+        "@assets/*": path.resolve("./src/assets/*")
+      }
+    }
+  }
 });
